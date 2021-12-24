d15f7b64f92f38d1d64af80036ed8ce3b30a1f37
Shreejan Pandey <81029735+NeplayGames@users.noreply.github.com>
2021-12-23 18:16:33 -0600

    Configure JWT auth and endpoint authorization

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -1,5 +1,6 @@
 using ClaimsManagementSystem.Contracts.Claims;
 using ClaimsManagementSystem.Services.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClaimsManagementSystem.Controllers;
@@ -15,17 +16,23 @@ public class ClaimsController : ControllerBase
         _claimsService = claimsService;
     }
 
+    [Authorize(Policy = "ClaimsReadAccess")]
     [HttpGet]
     [ProducesResponseType(typeof(PagedResponseDto<ClaimListItemResponseDto>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public async Task<ActionResult<PagedResponseDto<ClaimListItemResponseDto>>> GetClaims([FromQuery] ClaimListQueryRequestDto query, CancellationToken cancellationToken)
     {
         var claims = await _claimsService.GetClaimsAsync(query, cancellationToken);
         return Ok(claims);
     }
 
+    [Authorize(Policy = "ClaimsReadAccess")]
     [HttpGet("{id:guid}")]
     [ProducesResponseType(typeof(ClaimDetailsResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ClaimDetailsResponseDto>> GetClaimById(Guid id, CancellationToken cancellationToken)
     {
@@ -33,9 +40,12 @@ public class ClaimsController : ControllerBase
         return Ok(claim);
     }
 
+    [Authorize(Policy = "ClaimsCreateAccess")]
     [HttpPost]
     [ProducesResponseType(typeof(ClaimDetailsResponseDto), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<ActionResult<ClaimDetailsResponseDto>> CreateClaim([FromBody] CreateClaimRequestDto request, CancellationToken cancellationToken)
     {
@@ -43,9 +53,12 @@ public class ClaimsController : ControllerBase
         return CreatedAtAction(nameof(GetClaimById), new { id = claim.Id }, claim);
     }
 
+    [Authorize(Policy = "ClaimsUpdateAccess")]
     [HttpPut("{id:guid}")]
     [ProducesResponseType(typeof(ClaimDetailsResponseDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<ActionResult<ClaimDetailsResponseDto>> UpdateClaim(Guid id, [FromBody] UpdateClaimRequestDto request, CancellationToken cancellationToken)
@@ -54,8 +67,11 @@ public class ClaimsController : ControllerBase
         return Ok(claim);
     }
 
+    [Authorize(Policy = "RequireAdmin")]
     [HttpDelete("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteClaim(Guid id, CancellationToken cancellationToken)
     {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using ClaimsManagementSystem.Data;
 using ClaimsManagementSystem.Middleware;
 using ClaimsManagementSystem.Services.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +26,39 @@ builder.Services.AddDbContext<ClaimsManagementContext>(options =>
     options.UseSqlServer(connectionString);
 });
 
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+var jwtSigningKey = builder.Configuration["Jwt:SigningKey"];
+
+if (string.IsNullOrWhiteSpace(jwtIssuer) || string.IsNullOrWhiteSpace(jwtAudience) || string.IsNullOrWhiteSpace(jwtSigningKey))
+{
+    throw new InvalidOperationException("JWT configuration is missing. Please configure Jwt:Issuer, Jwt:Audience, and Jwt:SigningKey.");
+}
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSigningKey))
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdmin", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("ClaimsReadAccess", policy => policy.RequireRole("Admin", "Adjuster"));
+    options.AddPolicy("ClaimsCreateAccess", policy => policy.RequireRole("Admin", "Adjuster", "Customer"));
+    options.AddPolicy("ClaimsUpdateAccess", policy => policy.RequireRole("Admin", "Adjuster"));
+});
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -42,6 +78,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
